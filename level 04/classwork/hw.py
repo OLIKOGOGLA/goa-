3f5f15e240e53f2c5b611ea08695bcc34ca3e679
OLIKOGOGLA <olikogogla@gmail.com>
2025-02-10 22:46:05 +0400

    Add files via upload

---
+++ level 04/classwork/hw.py
@@ -0,0 +1,24 @@
+greeting = "გამარჯობა მსოფლიო"
+print(greeting)
+
+number = 42
+print(number)
+
+a = 10
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+x = 10
+print(x + 5)
+
+age = 25
+name = "ნიკა"
+
+print(name + " არის " + str(age) + " წლის")
---
+++ level 07/classwork/cw.py
@@ -0,0 +1,18 @@
+number = 10
+number_str = str(number)
+print(number_str)
+
+str_num1 = "25"
+str_num2 = "40"
+int_num1 = int(str_num1)
+int_num2 = int(str_num2)
+print(int_num1)
+print(int_num2)
+
+float_value = float(input("ჩაწერეთ რიცხვი (float): "))
+print(type(float_value))
+print(float_value)
+
+age = int(input("ჩაწერეთ თქვენი ასაკი: "))
+future_age = age + 4
+print(f"თქვენი ასაკი 4 წელში იქნება: {future_age}")
