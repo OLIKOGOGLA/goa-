ea060c25d07c3a8e30fa6bd15e189b07a3bdd2ca
OLIKOGOGLA <olikogogla@gmail.com>
2025-04-12 19:35:35 +0400

    Add files via upload

---
+++ level 43/claswork/cw.py
@@ -0,0 +1,5 @@
+# #https://www.codewars.com/kata/523b4ff7adca849afe000035/train/python
+# https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/python
+# https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/python,
+# https://www.codewars.com/kata/58261acb22be6e2ed800003a,
+# https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/python
