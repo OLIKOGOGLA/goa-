2b498021fdf90ae786f55fd5a47ccfee08b3ec16
OLIKOGOGLA <olikogogla@gmail.com>
2025-05-27 14:13:50 +0400

    Add files via upload

---
+++ level 18/level 15/clasework/cw.py
@@ -0,0 +1,13 @@
+# 1. ტემპერატურის შემოწმება
+temperature = int(input("შეიყვანეთ ტემპერატურა: "))
+if temperature > 30:
+    print("it's hot outside")
+else:
+    print("it's not too hot outside")
+
+# 2. ასაკის შემოწმება
+age = int(input("შეიყვანეთ თქვენი ასაკი: "))
+if age > 19:
+    print("adult")
+else:
+    print("teenager")
